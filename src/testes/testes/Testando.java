134a6f41c9029c49df8d1dc1e6bd7cde60036b68
anapauladasilvalins <apaulaslins@gmail.com>
2021-02-22 16:57:21 -0300

    Testando o git pela primeira vez

---
+++ src/testes/testes/Testando.java
@@ -0,0 +1,20 @@
+package testes.testes;
+
+import java.util.Scanner;
+
+public class Testando {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner nota = new Scanner(System.in);
+
+		int x=10, y=30, z=25, m=10;
+		
+		if((x!= (y-z))|| (y ==(z-5))){
+			m +=y;
+		}
+		System.out.println(m);
+	}
+	
+
+}
